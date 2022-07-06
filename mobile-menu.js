c7b9779b5b2329d35c431df38cc23d9a027a6909
uche ogwumike <ucheogwumike@gmail.com>
2022-07-06 06:29:44 +0100

    grid was added

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const primaryNav = document.querySelector('.navlinks-wrapper');
 const medButton = document.querySelector('.burger');
 
@@ -15,22 +14,4 @@ medButton.addEventListener("click",() => {
         medButton.setAttribute('aria-expanded',false);
     }
     
-=======
-const primaryNav = document.querySelector('.navlinks-wrapper');
-const medButton = document.querySelector('.burger');
-
-medButton.addEventListener("click",() => {
-    const visibile = primaryNav.getAttribute('data-visible');
-
-
-    if(visibile === "false"){
-    primaryNav.setAttribute('data-visible',true);
-    medButton.setAttribute('aria-expanded',true);
-}
-    else{
-        primaryNav.setAttribute('data-visible',false);
-        medButton.setAttribute('aria-expanded',false);
-    }
-    
->>>>>>> 9542d660aa6d9e5b99b69317bdd1ac8886f6d895
 });
